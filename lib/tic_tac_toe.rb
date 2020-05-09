3783e1a96d94330a923a4c8fadc5a36f24f60bb5
Matt Im <matthew.seiim@gmail.com>
2020-05-09 20:23:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,10 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board[index])
+  if index.between?(0,8) && !position_taken?(@board[index])
+    true
+  else
+    false
 end
 
 def turn(board)
