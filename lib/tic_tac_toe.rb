e8a4817cc0390aa0e51adedcab338808c78d360c
Matt Im <matthew.seiim@gmail.com>
2020-05-09 21:10:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board[index])
+  index.between?.to_i(0,8) && !position_taken?(@board[index])
 end
 
 def turn_count
