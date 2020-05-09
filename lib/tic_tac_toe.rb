ee11e6f97b426ddc139034499263e567b30387d9
Matt Im <matthew.seiim@gmail.com>
2020-05-09 20:21:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,20 +31,12 @@ def move(index, current_player = "X")
   @board[index] = current_player
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
 def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
 
 def valid_move?(index)
   if index.between?(0,8) && !position_taken?(@board[index])
-    true
-  else
-    false
-  end
 end
 
 def turn(board)
