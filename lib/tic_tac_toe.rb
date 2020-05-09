10764e4666a839b24f033d317bac4c14dc6ccbd5
Matt Im <matthew.seiim@gmail.com>
2020-05-09 20:11:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
 
-def valid_move?(@board[index])
+def valid_move?(index)
   index.between?(0,8) && !position_taken?(@board[index])
 end
 
