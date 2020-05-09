07d4014fa28c50699ce4e22ab2bb3b618da6656a
Matt Im <matthew.seiim@gmail.com>
2020-05-09 19:50:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(index, current_player = "X")
+  @board[index] = current_player
 end
 
 def valid_move?(board, index)
