5277672b7bba0376f11abc3aa4d0905cac2113ca
Matt Im <matthew.seiim@gmail.com>
2020-05-09 21:49:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ def turn
   index = input_to_index(input)
   if valid_move?(index)
     move(index, current_player(@board))
-    display_board(@board)
+    display_board
   else
     turn(@board)
   end
