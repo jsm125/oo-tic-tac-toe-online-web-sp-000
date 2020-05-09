35bacca756cda7ab802fd2221710697c28315a20
Matt Im <matthew.seiim@gmail.com>
2020-05-09 22:20:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,7 +78,15 @@ def won?
     position_1 = @board[win_index_1]
     position_2 = @board[win_index_2]
     position_3 = @board[win_index_3]
-  end
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+    }
+    return false
 end
   
 def full?
